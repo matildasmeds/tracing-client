8991f186ee7706c8e22e3dd6ed174f7a0060f277
Matilda Smeds <matilda.smeds@gmail.com>
2025-03-04 19:48:44 +0100

    With this I can record values, but too many values become recorded

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use tracing::{info, info_span, record_all};
+use tracing::Value;
+use tracing::{info, info_span};
 use tracing_core::dispatch::set_global_default;
 use tracing_subscriber::{prelude::*, FmtSubscriber, Registry};
 
@@ -10,6 +11,26 @@ fn main() {
     let span = info_span!("My span...", field1 = 1, field2 = 2);
     let _guard = span.enter();
     info!("Let's call the macro!");
-    record_all!(span, field1 = "One", field2 = "Two");
+
+    // The hope here is, that if I manage to build the ValueSet and call
+    // record_all in regular Rust code, I can probably manage to wrap it
+    // up in a macro too. However, the results are a bit surprising.
+    // New fields are added to the span, not replaced.
+    let meta = span.metadata().unwrap();
+    let field_set = meta.fields();
+    let values = &[
+        (
+            &field_set.field("field1").unwrap(),
+            Some(&"One" as &dyn Value),
+        ),
+        (
+            &field_set.field("field2").unwrap(),
+            Some(&"Two" as &dyn Value),
+        ),
+    ];
+    let value_set = field_set.value_set(values);
+
+    span.record_all(&value_set);
+
     info!("Macro has been called!");
 }
